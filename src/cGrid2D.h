76b634dfd9d50437fe9abfde319de5715c948465
JamesBremner <james@ravenspoint.com>
2024-06-12 19:43:51 -0400

    code

---
+++ src/cGrid2D.h
@@ -0,0 +1,342 @@
+///  graph with vertices on a 2D grid
+class cGrid2D
+{
+public:
+    typedef std::pair<int, int> point_t;
+    typedef std::pair<point_t, point_t> edge_t;
+    typedef std::vector<point_t> path_t;
+
+    cGrid2D()
+    {
+        reset();
+    }
+    void reset()
+    {
+        myColCount = -1;
+        myRowCount = -1;
+    }
+
+    /// @brief Extend grid dimensions if neccessary to include point
+    /// @param x zero based col
+    /// @param y zero based row
+
+    void extDim(int x, int y)
+    {
+        if (x < 0 || y < 0)
+            throw std::runtime_error(
+                "Bad cGrid2D co-ords");
+        if (x > myColCount - 1)
+            myColCount = x + 1;
+        if (y > myRowCount - 1)
+            myRowCount = y + 1;
+    }
+
+    /// @brief set grid dimensions
+    /// @param colCount
+    /// @param rowCount
+
+    void setDim(int colCount, int rowCount)
+    {
+        myColCount = colCount;
+        myRowCount = rowCount;
+    }
+
+    /// @brief Add edge between vertices
+    /// @param src vertex index
+    /// @param dst vertex index
+    void addEdge(int src, int dst)
+    {
+        if (src < 0 || dst < 0)
+            throw std::runtime_error(
+                "cGrid2D::addEdge bad index");
+        vEdge.push_back(std::make_pair(src, dst));
+    }
+    void addOrthoEdges()
+    {
+        for (int row = 0; row < myRowCount - 1; row++)
+            for (int col = 0; col < myColCount - 1; col++)
+            {
+                addEdge(index(col, row), index(col + 1, row));
+                addEdge(index(col, row), index(col, row + 1));
+            }
+        for (int col = 0; col < myColCount - 1; col++)
+        {
+            addEdge(index(col, myRowCount - 1), index(col + 1, myRowCount - 1));
+        }
+    }
+    void addDiagEdges()
+    {
+        for (int row = 0; row < myRowCount - 1; row++)
+            for (int col = 1; col < myColCount - 1; col++)
+            {
+                addEdge(index(col, row), index(col - 1, row + 1));
+                addEdge(index(col, row), index(col + 1, row + 1));
+            }
+        for (int row = 0; row < myRowCount - 1; row++)
+        {
+            addEdge(index(0, row), index(1, row + 1));
+            addEdge(index(myColCount - 1, row), index(myColCount - 2, row + 1));
+        }
+    }
+
+    void addDownEdges()
+    {
+        for (int row = 0; row < myRowCount - 1; row++) {
+            addEdge(index(0,row),index(0,row+1));
+            addEdge(index(0,row),index(1,row+1));
+            for (int col = 1; col < myColCount - 1; col++)
+            {
+                addEdge(index(col,row),index(col-1,row+1));
+                addEdge(index(col,row),index(col,row+1));
+                addEdge(index(col,row),index(col+1,row+1));
+            }
+            addEdge(index(myColCount - 1,row),index(myColCount-2,row+1));
+            addEdge(index(myColCount - 1,row),index(myColCount-1,row+1));
+        }
+    }
+
+        /// @brief get grid dimensions
+        /// @param[out] colCount
+        /// @param[out] rowCount
+
+        void getDim(int &colCount, int &rowCount) const
+        {
+            colCount = myColCount;
+            rowCount = myRowCount;
+        }
+
+        /// @brief get vertex index from location
+        /// @param col
+        /// @param row
+        /// @return vertex index, or -1
+
+        int index(int col, int row) const
+        {
+            if (0 > row || row >= myRowCount ||
+                0 > col || col >= myColCount)
+                return -1;
+            return row * myColCount + col;
+        }
+
+        /// @brief get vertex location from index
+        /// @param[out] col
+        /// @param[out] row
+        /// @param index
+
+        void coords(int &col, int &row, int index) const
+        {
+            row = index / myColCount;
+            col = index - row * myColCount;
+        }
+
+        /// @brief get point locations
+        /// @return vector of point locations
+
+        std::vector<point_t>
+        getPoints() const
+        {
+            std::vector<std::pair<int, int>> ret;
+            int x, y;
+            for (int p = 0; p < myColCount * myRowCount; p++)
+            {
+                coords(x, y, p);
+                ret.push_back(std::make_pair(x, y));
+            }
+            return ret;
+        }
+
+        std::vector<int>
+        getOrthoNeighboursRightBelow(int cell)
+        {
+            std::vector<int> ret;
+            int col, row;
+            coords(col, row, cell);
+            int ncol, nrow;
+            ncol = col + 1;
+            nrow = row;
+            if (ncol < myColCount && nrow < myRowCount)
+                ret.push_back(index(ncol, nrow));
+            ncol = col;
+            nrow = row + 1;
+            if (ncol < myColCount && nrow < myRowCount)
+                ret.push_back(index(ncol, nrow));
+            ncol = col + 1;
+            nrow = row + 1;
+            if (ncol < myColCount && nrow < myRowCount)
+                ret.push_back(index(ncol, nrow));
+            return ret;
+        }
+
+        std::vector<int>
+        getOrthoDiagNeighboursRightBelow(int cell)
+        {
+            std::vector<int> ret;
+            int col, row;
+            coords(col, row, cell);
+            ret = getOrthoNeighboursRightBelow(cell);
+            int ncol = col + 1;
+            int nrow = row + 1;
+            if (ncol < myColCount && nrow < myRowCount)
+                ret.push_back(index(ncol, nrow));
+            return ret;
+        }
+
+        /// @brief get edge locations
+        /// @return vector of edge locations (src col,src row), (dst col, dst row )
+
+        std::vector<edge_t>
+        getEdges() const
+        {
+            std::vector<edge_t> ret;
+            for (auto &e : vEdge)
+            {
+                int x1, y1, x2, y2;
+                coords(x1, y1, e.first);
+                coords(x2, y2, e.second);
+                ret.push_back(
+                    std::make_pair(
+                        std::make_pair(x1, y1), std::make_pair(x2, y2)));
+            }
+            return ret;
+        }
+
+        std::string name(int c, int r) const
+        {
+            return "c" + std::to_string(c) + "r" + std::to_string(r);
+        }
+
+        std::string name(int cell) const
+        {
+            int c, r;
+            coords(c, r, cell);
+            if (0 > c || c >= myColCount &&
+                             0 > r && r >= myRowCount)
+                return "";
+            return name(c, r);
+        }
+
+        /// @brief get edge indices
+        /// @return vector of index pairs (v1 index, v2 index )
+        std::vector<std::pair<int, int>>
+        getEdgesVertexIndex() const
+        {
+            return vEdge;
+        }
+
+        void addEdges2Graph(raven::graph::cGraph & g)
+        {
+            for (auto &e : vEdge)
+            {
+                g.add(
+                    name(e.first),
+                    name(e.second));
+            }
+        }
+
+        /// @brief Manhatten path, horizontal part
+        /// @param s starting point
+        /// @param d destination point
+        /// @return path, including start
+        static path_t
+        X(const point_t &s, const point_t &d)
+        {
+            path_t ret;
+            ret.push_back(s);
+            int deltax = d.first - s.first;
+            if (deltax)
+            {
+                int inc = 1;
+                if (deltax < 0)
+                    inc = -1;
+                int x = s.first;
+                do
+                {
+                    x += inc;
+                    ret.push_back(std::make_pair(x, s.second));
+                } while (x != d.first);
+            }
+            return ret;
+        }
+
+        /// @brief Manhatten path, vertical part
+        static path_t
+        Y(const point_t &s, const point_t &d)
+        {
+            std::vector<std::pair<int, int>> ret;
+            ret.push_back(s);
+            int deltay = d.second - s.second;
+            if (deltay)
+            {
+                int inc = 1;
+                if (deltay < 0)
+                    inc = -1;
+                int y = s.second;
+                do
+                {
+                    y += inc;
+                    ret.push_back(std::make_pair(s.first, y));
+                } while (y != d.second);
+            }
+            return ret;
+        }
+
+        /// @brief Manhatten path, diagonal steps
+        static path_t
+        D(const point_t &s, const point_t &d)
+        {
+            std::vector<std::pair<int, int>> ret;
+            ret.push_back(s);
+            int deltax = d.first - s.first;
+            int deltay = d.second - s.second;
+            int incx = 1;
+            int incy = 1;
+            if (deltax < 0)
+                incx = -1;
+            if (deltay < 0)
+                incy = -1;
+            int x = s.first;
+            int y = s.second;
+            while ((x != d.first) || (y != d.second))
+            {
+                if (x == d.first)
+                    incx = 0;
+                if (y == d.second)
+                    incy = 0;
+                x += incx;
+                y += incy;
+                ret.push_back(std::make_pair(x, y));
+            }
+            return ret;
+        }
+
+        /// @brief Manhatten path, vertical than horizontal
+        static path_t
+        YX(const point_t &s, const point_t &d)
+        {
+            auto path = Y(s, d);
+            auto path2 = X(point_t(s.first, d.second), d);
+            path.insert(
+                path.end(),
+                path2.begin() + 1,
+                path2.end());
+            return path;
+        }
+
+        /// @brief Manhatten path, horizontal then vertical
+        static path_t
+        XY(const point_t &s, const point_t &d)
+        {
+            auto path = X(s, d);
+            auto path2 = Y(point_t(d.first, s.second), d);
+            path.insert(
+                path.end(),
+                path2.begin() + 1,
+                path2.end());
+            return path;
+        }
+
+    private:
+        int myColCount;
+        int myRowCount;
+        std::vector<std::pair<int, int>> vEdge;
+    };
--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,109 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
+#include "GraphTheory.h"
+#include "cGrid2D.h"
 
-class cGUI : public cStarterGUI
+static std::vector<std::string>
+tokenize(
+    const std::string &line)
 {
-public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
-    {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
+    std::vector<std::string> ret;
+    std::stringstream sst(line);
+    std::string a;
+    while (getline(sst, a, ' '))
+        ret.push_back(a);
+    return ret;
+}
 
-        show();
-        run();
+void findpath(std::istream &ifs)
+{
+    // A directed graph
+    raven::graph::sGraphData graphData;
+    graphData.g.clear();
+    graphData.g.directed();
+
+    // A 2D grid
+    cGrid2D grid;
+    std::vector<double> vHeight;
+
+    std::string line;
+    int colcount = -1;
+    int rowCount = 0;
+    int startRow, startCol, endRow, endCol;
+
+    // setup grid
+    while (getline(ifs, line))
+    {
+        auto vt = tokenize(line);
+
+        bool firstcol = true;
+        for (auto &e : vt)
+        {
+            if (firstcol)
+                firstcol = false;
+            else
+                vHeight.push_back(atof(e.c_str()));
+        }
+        if (colcount == -1)
+            colcount = vt.size() - 1;
+        else if (colcount != vt.size() - 1)
+            throw std::runtime_error(
+                "variable column count");
+        rowCount++;
+    }
+    grid.setDim(colcount, rowCount);
+
+    // Path can move only downwards
+    grid.addDownEdges();
+
+    // convert grid to graph with weighted edges
+    graphData.edgeWeight.clear();
+    for (auto &p : grid.getEdgesVertexIndex())
+    {
+        // add edges to graph with weight of the destination vertex
+        int ei = graphData.g.add(p.first, p.second);
+        graphData.edgeWeight.push_back(vHeight[p.second]);
+    }
+    // start from any vertex in top row
+    int startIndex = graphData.g.vertexCount();
+    for (int iv = 0; iv < colcount; iv++)
+    {
+        int ei = graphData.g.add(startIndex, iv);
+        graphData.edgeWeight.push_back(vHeight[iv]);
+    }
+    // end from any vertex in bottom row
+    int endIndex = graphData.g.vertexCount();
+    for (int iv = endIndex - 1 - colcount; iv < endIndex - 1; iv++)
+    {
+        int ei = graphData.g.add(iv, endIndex);
+        graphData.edgeWeight.push_back(0);
     }
 
-private:
-    wex::label &lb;
-};
+    // Apply Dijsktra
+    graphData.startName = graphData.g.userName(startIndex);
+    graphData.endName = graphData.g.userName(endIndex);
+    auto res = raven::graph::path(graphData);
+
+    // Display path found ( col, row )
+    int r, c;
+    for (int i = 1; i < res.first.size() - 1; i++)
+    {
+        grid.coords(c, r, res.first[i]);
+        std::cout << c << "," << r << " -> ";
+    }
+    std::cout << " total energy " << res.second << "\n";
+}
+
 
 main()
 {
-    cGUI theGUI;
+    std::string sin =
+        "o 1 2 3\n"
+        "o 2 3 1\n"
+        "o 3 2 1\n";
+    std::istringstream ifs(sin);
+    findpath(ifs);
+
     return 0;
 }
